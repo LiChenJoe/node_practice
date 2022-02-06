4c9c95bb4a94c71fe9af1a0cc810d7a40fb46b93
User <user@UserdeMacBook-Air.local>
2022-02-06 21:54:18 +0800

    frontpage setting

---
+++ app.js
@@ -0,0 +1,19 @@
+const express= require("express");
+const app = express();
+const bodyParser=require("body-parser")
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("Public"));
+
+app.get("/", (req,res)=>{
+   res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/result", (req,res)=>{
+    let {Name, Date} =req.body;
+    res.send(`${Name}, Thank you for sharing.`);
+})
+
+app.listen(3000, ()=>{
+    console.log("Port3000 is runing");
+});
